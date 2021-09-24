a1abd2d93fe0a4123af0df2d0bb74c09288bd706
Elizabeth <mphilz@mail.ru>
2021-09-24 13:29:19 +0300

    first commit

---
+++ HTTP_gin_server/go.mod
@@ -0,0 +1,5 @@
+module github.com/dopeelis/DEV_tasks/HTTP_gin_server
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.4
---
+++ HTTP_gin_server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	api := router.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			about := v1.Group("/about")
+			{
+				about.GET("/", getAbout)
+			}
+			user := v1.Group("/user")
+			{
+				user.GET("/:id", getUserByID)
+			}
+		}
+	}
+
+	router.Run(":8080")
+}
+
+func getAbout(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "this is 'about' page",
+	})
+}
+
+func getUserByID(ctx *gin.Context) {
+	ID, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(400, gin.H{
+			"error": "can't convert user ID",
+		})
+	}
+	if ID > 0 {
+		ctx.JSON(200, gin.H{
+			"message": fmt.Sprintf("this is the user: %v", ID),
+		})
+	} else {
+		ctx.JSON(406, gin.H{
+			"error": "user ID has invalid value",
+		})
+	}
+}
